a99b60088b3b1393c71e9bd75a974ecae424a555
thejini3 <thejini3@gmail.com>
2020-08-06 21:38:05 +0600

    meow

--- processing.go
+++ processing.go
@@ -36,8 +36,8 @@ func sldcToDailyData() {
 
 	folder := "SLDC_Data"
 	// outputTxt := "Date,Hour,Min,Load\n"
-	// outputTxt := "Date,Hour,Load\n"
-	outputTxt := "Date,Avg,Peak\n"
+	outputTxt := "Date,Hour,Load\n"
+	// outputTxt := "Date,Avg,Peak\n"
 
 	err := filepath.Walk(folder, func(path string, info os.FileInfo, err error) error {
 		if info.IsDir() || filepath.Ext(path) != ".csv" {
@@ -60,33 +60,34 @@ func sldcToDailyData() {
 			if row.Value > peak {
 				peak = row.Value
 			}
-			// for hour := 0; hour <= 23; hour++ {
-			// 	// row.Time 00:00
-			// 	// hourMin [01, 00]
-			// 	hourMin := strings.Split(row.Time, ":")
-			// 	// minute string : 00
-			// 	if len(hourMin) != 2 {
-			// 		hel.Pl(hourMin)
-			// 		panic("Invalid hourMin")
-			// 	}
-			// 	minStr := hourMin[1]
-			// 	cond := timify(hour) == hourMin[0] && (minStr == "00" || minStr == "30")
-			// 	if cond {
-			// 		var min int
-			// 		if minStr == "00" {
-			// 			min = 0
-			// 		} else if minStr == "30" {
-			// 			min = 30
-			// 		} else {
-			// 			panic("Unknown minute: " + err.Error())
-			// 		}
-			// 		// outputTxt += fmt.Sprintf("%s,%d,%s\n", date, hour, row.Value)
-			// 		outputTxt += fmt.Sprintf("%s,%d,%d,%s\n", date, hour, min, row.Value)
-			// 	}
-			// }
+			for hour := 0; hour <= 23; hour++ {
+				// row.Time 00:00
+				// hourMin [01, 00]
+				hourMin := strings.Split(row.Time, ":")
+				// minute string : 00
+				if len(hourMin) != 2 {
+					hel.Pl(hourMin)
+					panic("Invalid hourMin")
+				}
+				minStr := hourMin[1]
+				cond := timify(hour) == hourMin[0] && (minStr == "00" || minStr == "30")
+				if cond {
+					// var min int
+					// if minStr == "00" {
+					// 	min = 0
+					// } else if minStr == "30" {
+					// 	min = 30
+					// } else {
+					// 	panic("Unknown minute: " + err.Error())
+					// }
+					// outputTxt += fmt.Sprintf("%s,%d,%d,%s\n", date, hour, min, row.Value)
+
+					outputTxt += fmt.Sprintf("%s,%d,%0.2f\n", date, hour, row.Value)
+				}
+			}
 		}
 
-		outputTxt += fmt.Sprintf("%s,%.2f,%.2f\n", date, total/count, peak)
+		// outputTxt += fmt.Sprintf("%s,%.2f,%.2f\n", date, total/count, peak)
 
 		return err
 	})
@@ -95,7 +96,11 @@ func sldcToDailyData() {
 		panic(err)
 	}
 
-	if err = hel.StrToFile("build/processed-avg-peak.csv", outputTxt); err != nil {
+	// if err = hel.StrToFile("build/processed-avg-peak.csv", outputTxt); err != nil {
+	// 	panic(err)
+	// }
+
+	if err = hel.StrToFile("build/processed-date-hour-load.csv", outputTxt); err != nil {
 		panic(err)
 	}
 }
